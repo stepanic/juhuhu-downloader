35076e772ff93f3504b349b78ff31ebbab882152
Matija Stepanic <stepanic.matija@gmail.com>
2022-10-10 15:33:36 +0200

    Introduced config

---
+++ config/paw-patrol.js
@@ -0,0 +1,65 @@
+const STREAMS = [
+    // { id: 'VCHMEdDvjkTnAXah', videos: 20, sid: '001', duration: 205, title: 'Filmska večer', },
+    { id: 'gYtGrvKbVgP8zKyh', videos: 22, sid: '002', duration: 225, title: 'Vulkanska erupcija' },
+    { id: '23Bw8fZU52HbcJCq', videos: 17, sid: '003', duration: 178, title: 'Ukrasna ploča' },
+    // { id: 'YYmvwemJcMbzgUjb', videos: 21, sid: '004', duration: 211, title: 'Kralj pauka' },
+    { id: 'FSFKNVpdzYxv9GG4', videos: 20, sid: '005', duration: 202, title: 'Majmonaut' },
+    { id: 'Bgg3HNu5Ppbz9uNj', videos: 19, sid: '006', duration: 200, title: 'Spašavanje mladunca' },
+    { id: '34nKgq9a8r6x6Q3W', videos: 18, sid: '007', duration: 184, title: 'Psići spašavaju ponija' },
+    { id: 'MFA6SREu3wMcaQDj', videos: 22, sid: '008', duration: 222, title: 'Kraljica majmuna' },
+    { id: 'WFnP5tcmaxabErnb', videos: 22, sid: '009', duration: 227, title: 'Ptičice' },
+    // { id: 'qytmxBHdcBx2vTHw', videos: 21, sid: '010', duration: 214, title: 'Robo pas' },
+    { id: 'JCB5kTg3exmNHQvV', videos: 22, sid: '011', duration: 227, title: 'Slomljeno krilo' },
+    // { id: 'ybWpftfxdnYz37ye', videos: 18, sid: '012', duration: 183, title: 'Zmaj' },
+    { id: 'A4yU2FzE2fH9H9bu', videos: 18, sid: '013', duration: 187, title: 'Opasnost mi je srednje ime' },
+    { id: '2bqEG9guyAGsJGCC', videos: 16, sid: '014', duration: 165, title: 'Problem s balončićem' },
+    { id: 'jtPNytdHtHKDKdDP', videos: 22, sid: '015', duration: 227, title: 'Poklon' },
+];
+
+exports.STREAMS = STREAMS;
+
+
+// Filmska vecer 03:25
+// https://streaming.hrt.hr/webstream/smil:VCHMEdDvjkTnAXah.smil/media_b2896000_20.ts
+
+// Vulkanska erupcija 03:45
+// https://streaming.hrt.hr/webstream/smil:gYtGrvKbVgP8zKyh.smil/media_b2896000_22.ts
+
+// Ukrasna ploca 02:58
+// https://streaming.hrt.hr/webstream/smil:23Bw8fZU52HbcJCq.smil/media_b2896000_17.ts
+
+// Kralj pauka 03:31
+// https://streaming.hrt.hr/webstream/smil:YYmvwemJcMbzgUjb.smil/media_b2896000_21.ts
+
+// Majmonaut 03:22
+// https://streaming.hrt.hr/webstream/smil:FSFKNVpdzYxv9GG4.smil/media_b2896000_20.ts
+
+// Spasavanje mladunca 03:20
+// https://streaming.hrt.hr/webstream/smil:Bgg3HNu5Ppbz9uNj.smil/media_b2896000_19.ts
+
+// Psici spasavaju ponija 03:04
+// https://streaming.hrt.hr/webstream/smil:34nKgq9a8r6x6Q3W.smil/media_b2896000_18.ts
+
+// Kraljica majmuna 03:42
+// https://streaming.hrt.hr/webstream/smil:MFA6SREu3wMcaQDj.smil/media_b2896000_22.ts
+
+// Pticice 03:47
+// https://streaming.hrt.hr/webstream/smil:WFnP5tcmaxabErnb.smil/media_b2896000_22.ts
+
+// Robo pas 03:34
+// https://streaming.hrt.hr/webstream/smil:qytmxBHdcBx2vTHw.smil/media_b2896000_21.ts
+
+// Slomljeno krilo 03:47
+// https://streaming.hrt.hr/webstream/smil:JCB5kTg3exmNHQvV.smil/media_b2896000_22.ts
+
+// Zmaj 03:03
+// https://streaming.hrt.hr/webstream/smil:ybWpftfxdnYz37ye.smil/media_b2896000_18.ts
+
+// Opasnost mi je srednje ime 03:07
+// https://streaming.hrt.hr/webstream/smil:A4yU2FzE2fH9H9bu.smil/media_b2896000_18.ts
+
+// Problem s baloncicem 02:45
+// https://streaming.hrt.hr/webstream/smil:2bqEG9guyAGsJGCC.smil/media_b2896000_16.ts
+ 
+// Poklon 03:47
+// https://streaming.hrt.hr/webstream/smil:jtPNytdHtHKDKdDP.smil/media_b2896000_22.ts
---
+++ config/pjesmice.js
@@ -0,0 +1,6 @@
+const STREAMS = [ 
+  { id: 'rEYcqnMhHNXzG7Sk', sid: '001', title: 'Tata', videos: 15, duration: 194 },
+  { id: 'qw9tmWvmVGW4cPcH', sid: '002', title: 'Papučice', videos: 15, duration: 193 },
+  { id: 'ypHzRajyvc3m4CxH', sid: '003', title: 'Koliko još?', videos: 14, duration: 184 },
+];
+exports.STREAMS = STREAMS;
